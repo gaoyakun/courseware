faf424dc4bafb57d25e7ba7e44fdc7be423568fe
gaoyakun <80844871@qq.com>
2018-08-15 19:22:59 +0800

    add Transition support

---
+++ web/js/sort.js
@@ -0,0 +1,6 @@
+Transition.setup ();
+Transition.setActivePage ($('#page-main'), 48);
+
+$('#to-what-is-sort').on ('click', function(){
+    Transition.setActivePage ($('#page-what-is-sort'), 48);
+});
