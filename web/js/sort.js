fa088a3cff91d96ca12ff636f5ecef9689c1730a
a a <aa@adeiMac.local>
2018-08-16 02:37:17 +0800

    Also change the page title if presentation page was changed

--- web/js/sort.js
+++ web/js/sort.js
@@ -1,26 +1,27 @@
-(function(){
+(function setupTransition(){
     Transition.setup ();
     Transition.setActivePage ($('#page-main'), 48);
     $('#to-what-is-sort').on ('click', function(){
         Transition.setActivePage ($('#page-what-is-sort'), 48);
+        $('#page-title').html('何为排序');
     });
+})();
 
-    (function(){
-        var values = [];
-        for (var i = 0; i < 60; i++) {
-            values.push (parseInt(Math.random()*100));
-        }
-        Graph.histogram ($('#fig-rand-unsort'), {
-            values:values,
-            paddingH:5
-        });
-        values.sort(function(a,b){
-            return a - b;
-        });
-        Graph.histogram ($('#fig-rand-sorted'), {
-            values:values,
-            paddingH:5
-        });
-    })();
+(function createRandFig(){
+    var values = [];
+    for (var i = 0; i < 60; i++) {
+        values.push (parseInt(Math.random()*100));
+    }
+    Graph.histogram ($('#fig-rand-unsort'), {
+        values:values,
+        paddingH:5
+    });
+    values.sort(function(a,b){
+        return a - b;
+    });
+    Graph.histogram ($('#fig-rand-sorted'), {
+        values:values,
+        paddingH:5
+    });
 })();
 
