97bd3d9986a40142dc0cc536ee2c5cb1403cf58a
gaoyakun <80844871@qq.com>
2018-09-27 18:02:56 +0800

    N/A

--- src/sort/sort.ts
+++ src/sort/sort.ts
@@ -1,14 +1,13 @@
 import $ from 'jquery';
-import {CousewareFramework} from '../lib/presentation';
-import {Graph} from '../lib/graph';
-import {DemoBase} from './demobase';
+import { CousewareFramework } from '../lib/presentation';
+import { Graph } from '../lib/graph';
+import { DemoBase } from './demobase';
 import { cwScene, cwApp } from '../lib/core';
 import { cwClickEvent } from '../lib/events';
 
 export class CourseSort extends CousewareFramework {
     private graphRandUnsort: Graph;
     private graphRandSorted: Graph;
-    //private bubbleDemo: NumberSequenceScene;
     private bubbleDemo: DemoBase;
 
     constructor () {
@@ -41,11 +40,37 @@ export class CourseSort extends CousewareFramework {
             if (evt.id == 'page-bubble-sort') {
                 cwScene.init ();
                 cwApp.run ();
-                this.bubbleDemo.start ([3,2,8,4,8,6,9,1,0,6,1,5,3],{
+                let numbers = [];
+                for (let i = 0; i < 12; i++) {
+                    numbers.push (Math.floor(Math.random()*10));
+                }
+                this.bubbleDemo.start (numbers,{
                     margin_h: 10,
                     margin_v: 60,
                     padding: 0
                 });
+                const pagefoot:HTMLDivElement = document.querySelector ('.page-foot');
+                const refresh:HTMLAnchorElement = document.createElement ('a');
+                const refreshImg:HTMLImageElement = document.createElement ('img');
+                refreshImg.src = 'images/refresh.png';
+                refresh.appendChild (refreshImg);
+                refresh.style.marginLeft = '10px';
+                refresh.style.cursor = 'pointer';
+                refresh.addEventListener ('click', ev=>{
+                    this.bubbleDemo.playShuffleDemo ();
+                });
+                pagefoot.insertBefore(refresh, pagefoot.firstChild);
+
+                const sort:HTMLAnchorElement = document.createElement ('a');
+                const sortImg:HTMLImageElement = document.createElement ('img');
+                sortImg.src = 'images/arrow_r.png';
+                sort.appendChild (sortImg);
+                sort.style.marginLeft = '60px';
+                sort.style.cursor = 'pointer';
+                sort.addEventListener ('click', ev=>{
+                    this.bubbleDemo.playBubbleSortDemo ();
+                });
+                pagefoot.insertBefore(sort, pagefoot.firstChild);
             }
         });        
         window.addEventListener ('pageOut', (evt:any)=>{
@@ -53,6 +78,9 @@ export class CourseSort extends CousewareFramework {
                 this.bubbleDemo.end ();
                 cwScene.done ();
                 cwApp.stop ();
+                const pagefoot:HTMLDivElement = document.querySelector ('.page-foot');
+                pagefoot.removeChild (pagefoot.firstChild);
+                pagefoot.removeChild (pagefoot.firstChild);
             }
         });     
     }
