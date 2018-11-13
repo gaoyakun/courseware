8cf28b7023591ae6621cb761192f510dad409d69
a a <aa@adeiMac.local>
2018-11-13 23:22:14 +0800

    N/A

--- src/playgrnd.ts
+++ src/playgrnd.ts
@@ -9,61 +9,57 @@ const toolFontSize = '18px';
 const PG = new playground.cwPlayground (document.querySelector('#playground-canvas'), true);
 const toolboxDiv: HTMLDivElement = document.querySelector('#toolbox');
 const toolbox = new pgeditor.cwPGEditorToolbox (toolboxDiv, PG, 'column');
-toolbox.loadTools ([
+const defaultTools = [
     {
         states: [{
             command: 'UseTool',
             iconClass: 'fas fa-mouse-pointer fa-fw',
-            color: '#888'
+            color: '#fff'
         },{
             command: 'UseTool name=PGTool_Select',
             iconClass: 'fas fa-mouse-pointer fa-fw',
-            color: '#00f'
+            color: '#fff'
         }],
         fontSize: toolFontSize
     }, {
         states: [{
             command: 'UseTool',
             iconClass: 'fas fa-exchange-alt fa-fw',
-            color: '#888'
+            color: '#fff'
         }, {
             command: 'UseTool name=PGTool_Swap',
             iconClass: 'fas fa-exchange-alt fa-fw',
-            color: '#00f'
-        }],
-        fontSize: toolFontSize
-    }, {
-        states: [{
-            command: 'CreateObject type=Label text=标签',
-            iconClass: 'fas fa-font fa-fw'
+            color: '#fff'
         }],
         fontSize: toolFontSize
     }
-]);
-const objectToolboxDiv: HTMLDivElement = document.querySelector('#object-toolbox');
-const objectToolbox = new pgeditor.cwPGEditorToolbox (objectToolboxDiv, PG, 'row');
+];
+toolbox.loadTools (defaultTools);
+
 PG.on (tools.cwPGObjectSelectedEvent.type, (ev: tools.cwPGObjectSelectedEvent) => {
     switch (ev.object.entityType) {
     case 'Label':
-        objectToolbox.loadTools ([{
+        toolbox.unloadTools ();
+        toolbox.loadTools (defaultTools);
+        toolbox.loadTools ([{
             states: [{
                 command: 'beginEdit',
                 iconClass: 'fas fa-edit fa-fw',
-                color: '#00f'
+                color: '#fff'
             }],
             fontSize: toolFontSize
         },{
             states: [{
                 command: 'fontScaleUp step=2',
                 iconClass: 'fas fa-plus fa-fw',
-                color: '#00f'
+                color: '#fff'
             }],
             fontSize: toolFontSize
         },{
             states: [{
                 command: 'fontScaleDown step=2',
                 iconClass: 'fas fa-minus fa-fw',
-                color: '#00f'
+                color: '#fff'
             }],
             fontSize: toolFontSize
         }]);
@@ -71,9 +67,36 @@ PG.on (tools.cwPGObjectSelectedEvent.type, (ev: tools.cwPGObjectSelectedEvent) =
     }
 });
 PG.on (tools.cwPGObjectDeselectedEvent.type, (ev: tools.cwPGObjectDeselectedEvent) => {
-    objectToolbox.unloadTools ();
+    toolbox.unloadTools ();
+    toolbox.loadTools (defaultTools);
 });
 
+const objectToolboxDiv: HTMLDivElement = document.querySelector('#object-toolbox');
+const objectToolbox = new pgeditor.cwPGEditorToolbox (objectToolboxDiv, PG, 'row');
+objectToolbox.loadTools ([{
+    states: [{
+        command: 'CreateObject type=Label text=标签',
+        iconClass: 'fas fa-font fa-fw',
+        color: '#fff'
+    }],
+    fontSize: toolFontSize
+}]);
+
+const commonToolboxDiv: HTMLDivElement = document.querySelector('#common-toolbox');
+const commonToolbox = new pgeditor.cwPGEditorToolbox (commonToolboxDiv, PG, 'column');
+commonToolbox.loadTools ([{
+    states: [{
+        command: '$StrokeColor',
+        iconClass: ''
+    }],
+    fontSize: toolFontSize
+},{
+    states: [{
+        command: '$FillColor',
+        iconClass: ''
+    }],
+    fontSize: toolFontSize
+}]);
 cwApp.run ();
 
 
--- src/playground/editor/toolbox.ts
+++ src/playground/editor/toolbox.ts
@@ -4,7 +4,7 @@ import * as commands from '../commands';
 export interface IToolDef {
     states: Array<{
         command: string;
-        iconClass: string;
+        iconClass?: string;
         color?: string;
     }>;
     fontSize: string;
@@ -14,12 +14,18 @@ export class cwPGEditorToolbox {
     private _container: HTMLDivElement;
     private _pg: playground.cwPlayground;
     private _tools: Array<IToolDef>;
+    private _curTool: IToolDef;
     private _direction: string;
+    private _strokeColor: string;
+    private _fillColor: string;
     constructor (container: HTMLDivElement, pg: playground.cwPlayground, direction: string) {
         this._container = container;
         this._pg = pg;
         this._tools = [];
+        this._curTool = null;
         this._direction = direction;
+        this._strokeColor = '#000000';
+        this._fillColor = '#ffffff';
         this.create (this._container);
     }
     create (container: HTMLDivElement) {
@@ -34,29 +40,60 @@ export class cwPGEditorToolbox {
     loadTools (tools: Array<IToolDef>) {
         tools.forEach ((tool: IToolDef) => {
             this._tools.push (tool);
-            const toolIcon: HTMLElement = document.createElement ('i');
-            toolIcon.style.fontSize = tool.fontSize || '60px';
-            toolIcon.style.lineHeight = tool.fontSize || '60px';
-            if (this._direction === 'column') {
-                toolIcon.style.marginBottom = '10px';
-            }
-            toolIcon.setAttribute ('toolIndex', String(this._tools.length-1));
-            toolIcon.setAttribute ('togglable', tool.states.length > 1 ? 'true' : 'false');
-            toolIcon.setAttribute ('toggleState', '0');
-            this.applyToolStyles (toolIcon);
-
-            this._container.appendChild (toolIcon);
-            toolIcon.addEventListener ('click', () => {
-                const togglable = toolIcon.getAttribute ('togglable');
-                let toggleState = Number(toolIcon.getAttribute ('toggleState'));
-                if (togglable == 'true') {
-                    toggleState = 1 - Number(toolIcon.getAttribute ('toggleState'));
+            const buttonSize = parseInt(tool.fontSize || '60') + 10; 
+            if (tool.states[0].command === '$StrokeColor') {
+                const inputBox: HTMLInputElement = document.createElement ('input');
+                inputBox.type = 'color';
+                inputBox.value = this._strokeColor;
+                inputBox.style.width = `${buttonSize}px`;
+                inputBox.style.height = `${buttonSize}px`;
+                inputBox.style.margin = '2px';
+                inputBox.onchange = () => {
+                    this._strokeColor = inputBox.value;
                 }
-                const toolIndex = Number(toolIcon.getAttribute ('toolIndex'));
-                toolIcon.setAttribute ('toggleState', String(toggleState));
+                this._container.appendChild (inputBox);
+            } else if (tool.states[0].command === '$FillColor') {
+                const inputBox: HTMLInputElement = document.createElement ('input');
+                inputBox.type = 'color';
+                inputBox.value = this._fillColor;
+                inputBox.style.width = `${buttonSize}px`;
+                inputBox.style.height = `${buttonSize}px`;
+                inputBox.style.margin = '2px';
+                inputBox.onchange = () => {
+                    this._fillColor = inputBox.value;
+                }
+                this._container.appendChild (inputBox);
+            } else {
+                const toolButton: HTMLElement = document.createElement ('div');
+                toolButton.classList.add ('flex-h', 'flex-align-x-center', 'flex-align-y-center');
+                toolButton.style.backgroundColor = '#444';
+                toolButton.style.width = `${buttonSize}px`;
+                toolButton.style.height = `${buttonSize}px`;
+                toolButton.style.borderRadius = '3px';
+                toolButton.style.border = '2px solid #fff';
+                toolButton.style.margin = '2px';
+                const toolIcon: HTMLElement = document.createElement ('i');
+                toolIcon.style.fontSize = tool.fontSize || '60px';
+                toolIcon.style.lineHeight = tool.fontSize || '60px';
+                toolIcon.setAttribute ('toolIndex', String(this._tools.length-1));
+                toolIcon.setAttribute ('togglable', tool.states.length > 1 ? 'true' : 'false');
+                toolIcon.setAttribute ('toggleState', '0');
                 this.applyToolStyles (toolIcon);
-                this._pg.executeCommand (commands.cwPGCommandParser.parse(this._tools[toolIndex].states[toggleState].command));
-            });
+
+                toolButton.appendChild (toolIcon);
+                this._container.appendChild (toolButton);
+                toolButton.addEventListener ('click', () => {
+                    const togglable = toolIcon.getAttribute ('togglable');
+                    let toggleState = Number(toolIcon.getAttribute ('toggleState'));
+                    if (togglable == 'true') {
+                        toggleState = 1 - Number(toolIcon.getAttribute ('toggleState'));
+                    }
+                    const toolIndex = Number(toolIcon.getAttribute ('toolIndex'));
+                    toolIcon.setAttribute ('toggleState', String(toggleState));
+                    this.applyToolStyles (toolIcon);
+                    this._pg.executeCommand (commands.cwPGCommandParser.parse(this._tools[toolIndex].states[toggleState].command));
+                });
+            }
         });
     }
     unloadTools () {
@@ -68,9 +105,11 @@ export class cwPGEditorToolbox {
     private applyToolStyles (toolIcon: HTMLElement) {
         const index = Number(toolIcon.getAttribute ('toggleState'));
         const toolIndex = Number(toolIcon.getAttribute ('toolIndex'));
-        this._tools[toolIndex].states[index].iconClass.split (' ').forEach ((token: string) => {
-            toolIcon.classList.add (token);
-        });
+        if (this._tools[toolIndex].states[index].iconClass) {
+            this._tools[toolIndex].states[index].iconClass.split (' ').forEach ((token: string) => {
+                toolIcon.classList.add (token);
+            });
+        }
         toolIcon.style.color = this._tools[toolIndex].states[index].color || '#888';
     }
 }
